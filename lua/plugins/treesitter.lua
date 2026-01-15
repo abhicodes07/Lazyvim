ce2c22a3a8355358e7c2f20017f3f3e3057eb823
Abhinav <abhiasthana76@gmail.com>
2026-01-15 19:09:51 +0530

    feat(code): :sparkles: add few treesitters

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    ensure_installed = { "html", "python", "jinja", "htmldjango" }, -- Add jinja2
+    ensure_installed = { "html", "python", "jinja", "htmldjango", "jinja_inline", "git_config", "gitattributes", "cpp" }, -- Add jinja2
     highlight = { enable = true },
   },
 }
