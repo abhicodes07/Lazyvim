6c2fe634199866c320a606bd9309a1cf66c89359
Abhinav <abhiasthana76@gmail.com>
2026-01-27 18:39:04 +0530

    refactor(noice): :recycle: change cmdline icon

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -6,31 +6,54 @@ return {
   opts = {
     -- add any options here
     -- you can enable a preset for easier configuration
+    cmdline = {
+      enabled = true, -- enables the Noice cmdline UI
+      view = "cmdline_popup", -- view for rendering the cmdline. Change to `cmdline` to get a classic cmdline at the bottom
+      opts = {}, -- global options for the cmdline. See section on views
+      ---@type table<string, CmdlineFormat>
+      format = {
+        -- conceal: (default=true) This will hide the text in the cmdline that matches the pattern.
+        -- view: (default is cmdline view)
+        -- opts: any options passed to the view
+        -- icon_hl_group: optional hl_group for the icon
+        -- title: set to anything or empty string to hide
+        cmdline = { pattern = "^:", icon = "󰘳", lang = "vim" },
+        search_down = { kind = "search", pattern = "^/", icon = " ", lang = "regex" },
+        search_up = { kind = "search", pattern = "^%?", icon = " ", lang = "regex" },
+        filter = { pattern = "^:%s*!", icon = "", lang = "bash" },
+        lua = { pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" }, icon = "", lang = "lua" },
+        help = { pattern = "^:%s*he?l?p?%s+", icon = "" },
+        input = { view = "cmdline_input", icon = "󰥻 " }, -- Used by input()
+        -- lua = false, -- to disable a format, set to `false`
+      },
+    },
     presets = {
       bottom_search = true, -- use a classic bottom cmdline for search
       command_palette = true, -- position the cmdline and popupmenu together
       long_message_to_split = true, -- long messages will be sent to a split
-      inc_rename = false, -- enables an input dialog for inc-rename.nvim
+      inc_rename = true, -- enables an input dialog for inc-rename.nvim
       lsp_doc_border = true, -- add a border to hover docs and signature help
     },
-  },
-  lsp = {
-    signature = {
-      enabled = false,
-      auto_open = {
-        enabled = true,
-        trigger = true, -- Automatically show signature help when typing a trigger character from the LSP
-        luasnip = true, -- Will open signature help when jumping to Luasnip insert nodes
-        throttle = 50, -- Debounce lsp signature help request by 50ms
+    lsp = {
+      progress = {
+        view = "mini",
       },
-      view = nil, -- when nil, use defaults from documentation
-      ---@type NoiceViewOptions
-      opts = {}, -- merged with defaults from documentation
+      signature = {
+        enabled = false,
+        auto_open = {
+          enabled = true,
+          trigger = true, -- Automatically show signature help when typing a trigger character from the LSP
+          luasnip = true, -- Will open signature help when jumping to Luasnip insert nodes
+          throttle = 50, -- Debounce lsp signature help request by 50ms
+        },
+        view = nil, -- when nil, use defaults from documentation
+        ---@type NoiceViewOptions
+        opts = {}, -- merged with defaults from documentation
+      },
     },
-  },
-  config = {
     notify = {
-      enabled = false,
+      enabled = true,
+      view = "notify",
     },
   },
   dependencies = {
